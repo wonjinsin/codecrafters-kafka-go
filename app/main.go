e12cc93394f3761755df13d8e1954659707a9aa2
wonjinsin <slgktn92@gmail.com>
2025-09-12 22:11:06 +0900

    Bind to a port #vi6

--- app/main.go
+++ app/main.go
@@ -1,15 +1,33 @@
 package main
 
 import (
+	"bytes"
+	"encoding/binary"
 	"fmt"
 	"io"
 	"net"
 	"os"
 )
 
-// Ensures gofmt doesn't remove the "net" and "os" imports in stage 1 (feel free to remove this!)
-var _ = net.Listen
-var _ = os.Exit
+type RequestV2 struct {
+	Message int32
+	Header  *RequestHeaderV2
+}
+
+type RequestHeaderV2 struct {
+	RequestAPIKey     int16
+	RequestAPIVersion int16
+	CorrelationID     int32
+}
+
+type ResponseV2 struct {
+	Message int32
+	Header  *ResponseHeaderV2
+}
+
+type ResponseHeaderV2 struct {
+	CorrelationID int32
+}
 
 func main() {
 	l, err := net.Listen("tcp", "0.0.0.0:9092")
@@ -30,16 +48,34 @@ func main() {
 
 func response(conn net.Conn) {
 	defer conn.Close()
+
 	for {
 		buffer := make([]byte, 1024)
-		_, err := conn.Read(buffer)
+		n, err := conn.Read(buffer)
 		if err != nil && err == io.EOF {
 			break
 		}
 
-		b := []byte{0, 0, 0, 0}
-		b = append(b, buffer[8:12]...)
+		reader := bytes.NewReader(buffer[:n])
 
-		conn.Write(b)
+		request := new(RequestV2)
+		requestHeader := new(RequestHeaderV2)
+		request.Header = requestHeader
+		binary.Read(reader, binary.BigEndian, &request.Message)
+		binary.Read(reader, binary.BigEndian, &requestHeader.RequestAPIKey)
+		binary.Read(reader, binary.BigEndian, &requestHeader.RequestAPIVersion)
+		binary.Read(reader, binary.BigEndian, &requestHeader.CorrelationID)
+
+		response := new(ResponseV2)
+		responseHeader := new(ResponseHeaderV2)
+		response.Header = responseHeader
+		response.Header.CorrelationID = request.Header.CorrelationID
+
+		binary.Write(conn, binary.BigEndian, response.Message)
+		binary.Write(conn, binary.BigEndian, response.Header.CorrelationID)
+		binary.Write(conn, binary.BigEndian, int16(35))
+
+		fmt.Println("write called")
+
 	}
 }
