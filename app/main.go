e0a71bfa6ab9f32191d567975655b529a2416708
wonjinsin <slgktn92@gmail.com>
2025-09-21 23:41:38 +0900

    List for an unknown topic #vt6

--- app/main.go
+++ app/main.go
@@ -19,9 +19,10 @@ func NewRequest(b []byte) *Request {
 	r := new(Request)
 	r.Message = int32(binary.BigEndian.Uint32(b[:4]))
 
-	header, _ := NewRequestHeader(b[4:])
+	header, lastIdx := NewRequestHeader(b[4:])
 	r.Header = header
 
+	r.Body = NewRequestBody(b[lastIdx+5:])
 	return r
 }
 
@@ -33,20 +34,20 @@ type RequestHeader struct {
 	TagBuffer         int8
 }
 
-func NewRequestHeader(b []byte) (r *RequestHeader, lastIndex int32) {
-	r = new(RequestHeader)
+func NewRequestHeader(b []byte) (*RequestHeader, int32) {
+	r := new(RequestHeader)
 	r.RequestAPIKey = int16(binary.BigEndian.Uint16(b[:2]))
 	r.RequestAPIVersion = int16(binary.BigEndian.Uint16(b[2:4]))
 	r.CorrelationID = int32(binary.BigEndian.Uint32(b[4:8]))
-	lastIdx := int32(8)
+	nextIdx := int32(7 + 1)
 
-	clientID, idx := NewRequestClientID(b[8:])
+	clientID, idx := NewRequestClientID(b[nextIdx:])
 	r.ClientID = clientID
-	lastIdx += idx
+	nextIdx += idx + 1
 
-	r.TagBuffer = int8(b[lastIdx])
+	r.TagBuffer = int8(b[nextIdx])
 
-	return r, lastIndex + 1
+	return r, nextIdx
 }
 
 type RequestHeaderClientID struct {
@@ -60,24 +61,68 @@ func NewRequestClientID(b []byte) (r *RequestHeaderClientID, lastIdx int32) {
 	if r.Length == 0 {
 		return r, 2
 	}
-	r.Contents = string(b[2:r.Length])
-	return r, int32(2 + r.Length)
+	r.Contents = string(b[2 : r.Length+2])
+	return r, int32(r.Length + 2 - 1)
 }
 
 type RequestBody struct {
-	ClientID        RequestBodyClientID
-	SoftwareVersion RequestBodySoftwareVersion
-	TagBuffer       int8
+	TopicsArray  *RequestTopicArray
+	PartionLimit int32
+	Cursor       *int8
+	TagBuffer    int8
 }
 
-type RequestBodyClientID struct {
-	Length   int8
-	Contents string
+func NewRequestBody(b []byte) (r *RequestBody) {
+	r = new(RequestBody)
+	topicArray, lastIdx := NewRequestTopicArray(b)
+	r.TopicsArray = topicArray
+	r.PartionLimit = int32(binary.BigEndian.Uint32(b[lastIdx+1 : lastIdx+5]))
+	var cursor *int8
+	if int8(b[lastIdx+5]) >= 0 {
+		c := int8(b[lastIdx+5])
+		cursor = &c
+	}
+	r.Cursor = cursor
+	r.TagBuffer = int8(b[lastIdx+6])
+	return r
 }
 
-type RequestBodySoftwareVersion struct {
-	Length   int8
-	Contents string
+type RequestTopicArray struct {
+	Length int8
+	Topics RequestTopics
+}
+
+func NewRequestTopicArray(b []byte) (r *RequestTopicArray, lastIdx int32) {
+	r = new(RequestTopicArray)
+	r.Length = int8(b[0])
+	r.Topics, lastIdx = NewRequestTopics(r.Length-1, b[1:])
+	return r, lastIdx
+}
+
+type RequestTopic struct {
+	NameLength int8
+	Name       string
+	TagBuffer  int8
+}
+
+func NewRequestTopic(b []byte) (r *RequestTopic, lastIdx int32) {
+	r = new(RequestTopic)
+	r.NameLength = int8(b[0])
+	r.Name = string(b[1:r.NameLength])
+	r.TagBuffer = int8(b[r.NameLength])
+	return r, int32(r.NameLength)
+}
+
+type RequestTopics []*RequestTopic
+
+func NewRequestTopics(length int8, b []byte) (r RequestTopics, lastIdx int32) {
+	nextIdx := int32(0)
+	for range length {
+		t, topicIdx := NewRequestTopic(b[nextIdx:])
+		r = append(r, t)
+		nextIdx += topicIdx + 1
+	}
+	return r, nextIdx
 }
 
 type Response struct {
@@ -87,33 +132,33 @@ type Response struct {
 }
 
 func NewResponse(r *Request) *Response {
-	errorCode := int16(0)
-	if r.Header.RequestAPIVersion < 0 || r.Header.RequestAPIVersion > 4 {
-		errorCode = 35
-	}
 	return &Response{
-		Message: 33,
+		Message: 41,
 		Header: &ResponseHeader{
 			CorrelationID: r.Header.CorrelationID,
+			TagBuffer:     0,
 		},
 		Body: &ResponseBody{
-			ErrorCode: errorCode,
-			VersionsArray: ResponseBodyVersionsArray{
-				Length: 4,
-				Versions: ResponseBodyVersions{
-					&ResponseBodyVersion{
-						APIKey:     1,
-						MaxVersion: 17,
-					},
-					&ResponseBodyVersion{
-						APIKey:     18,
-						MaxVersion: 4,
-					},
-					&ResponseBodyVersion{
-						APIKey: 75,
+			ThrottleTime: 41,
+			TopicsArray: ResponseBodyTopicsArray{
+				Length: 2,
+				Topics: ResponseBodyTopics{
+					&ResponseBodyTopic{
+						ErrorCode: 3,
+						TopicName: &ResponseBodyTopicName{
+							Length:   4,
+							Contents: "Foo",
+						},
+						TopicID:                   [16]byte{},
+						IsInternal:                0,
+						PartitionsArray:           1,
+						TopicAuthorizedOperations: 3576,
+						TagBuffer:                 0,
 					},
 				},
 			},
+			NextCursor: -1,
+			TagBuffer:  0,
 		},
 	}
 }
@@ -123,49 +168,55 @@ func (r Response) Parse() []byte {
 
 	binary.Write(&buf, binary.BigEndian, r.Message)
 	binary.Write(&buf, binary.BigEndian, r.Header.CorrelationID)
-	binary.Write(&buf, binary.BigEndian, r.Body.ErrorCode)
-	binary.Write(&buf, binary.BigEndian, r.Body.VersionsArray.Length)
-	binary.Write(&buf, binary.BigEndian, r.Body.VersionsArray.Versions[0].APIKey)
-	binary.Write(&buf, binary.BigEndian, r.Body.VersionsArray.Versions[0].MinVersion)
-	binary.Write(&buf, binary.BigEndian, r.Body.VersionsArray.Versions[0].MaxVersion)
-	binary.Write(&buf, binary.BigEndian, r.Body.VersionsArray.Versions[0].TagBuffer)
-	binary.Write(&buf, binary.BigEndian, r.Body.VersionsArray.Versions[1].APIKey)
-	binary.Write(&buf, binary.BigEndian, r.Body.VersionsArray.Versions[1].MinVersion)
-	binary.Write(&buf, binary.BigEndian, r.Body.VersionsArray.Versions[1].MaxVersion)
-	binary.Write(&buf, binary.BigEndian, r.Body.VersionsArray.Versions[1].TagBuffer)
-	binary.Write(&buf, binary.BigEndian, r.Body.VersionsArray.Versions[2].APIKey)
-	binary.Write(&buf, binary.BigEndian, r.Body.VersionsArray.Versions[2].MinVersion)
-	binary.Write(&buf, binary.BigEndian, r.Body.VersionsArray.Versions[2].MaxVersion)
-	binary.Write(&buf, binary.BigEndian, r.Body.VersionsArray.Versions[2].TagBuffer)
+	binary.Write(&buf, binary.BigEndian, r.Header.TagBuffer)
 	binary.Write(&buf, binary.BigEndian, r.Body.ThrottleTime)
+	binary.Write(&buf, binary.BigEndian, r.Body.TopicsArray.Length)
+	binary.Write(&buf, binary.BigEndian, r.Body.TopicsArray.Topics[0].ErrorCode)
+	binary.Write(&buf, binary.BigEndian, r.Body.TopicsArray.Topics[0].TopicName.Length)
+	buf.Write([]byte(r.Body.TopicsArray.Topics[0].TopicName.Contents))
+	buf.Write(r.Body.TopicsArray.Topics[0].TopicID[:])
+	binary.Write(&buf, binary.BigEndian, r.Body.TopicsArray.Topics[0].IsInternal)
+	binary.Write(&buf, binary.BigEndian, r.Body.TopicsArray.Topics[0].PartitionsArray)
+	binary.Write(&buf, binary.BigEndian, r.Body.TopicsArray.Topics[0].TopicAuthorizedOperations)
+	binary.Write(&buf, binary.BigEndian, r.Body.TopicsArray.Topics[0].TagBuffer)
+	binary.Write(&buf, binary.BigEndian, r.Body.NextCursor)
 	binary.Write(&buf, binary.BigEndian, r.Body.TagBuffer)
 	return buf.Bytes()
 }
 
 type ResponseHeader struct {
 	CorrelationID int32
-}
-
-type ResponseBody struct {
-	ErrorCode     int16
-	VersionsArray ResponseBodyVersionsArray
-	ThrottleTime  int32
 	TagBuffer     int8
 }
 
-type ResponseBodyVersionsArray struct {
-	Length   int8
-	Versions ResponseBodyVersions
+type ResponseBody struct {
+	ThrottleTime int32
+	TopicsArray  ResponseBodyTopicsArray
+	NextCursor   int8
+	TagBuffer    int8
 }
 
-type ResponseBodyVersion struct {
-	APIKey     int16
-	MinVersion int16
-	MaxVersion int16
-	TagBuffer  int8
+type ResponseBodyTopicsArray struct {
+	Length int8
+	Topics ResponseBodyTopics
 }
 
-type ResponseBodyVersions []*ResponseBodyVersion
+type ResponseBodyTopic struct {
+	ErrorCode                 int16
+	TopicName                 *ResponseBodyTopicName
+	TopicID                   [16]byte
+	IsInternal                int8
+	PartitionsArray           int8
+	TopicAuthorizedOperations int32
+	TagBuffer                 int8
+}
+
+type ResponseBodyTopics []*ResponseBodyTopic
+
+type ResponseBodyTopicName struct {
+	Length   int32
+	Contents string
+}
 
 func main() {
 	l, err := net.Listen("tcp", "0.0.0.0:9092")
@@ -195,8 +246,9 @@ func response(conn net.Conn) {
 		}
 
 		request := NewRequest(buffer[:n])
-		response := NewResponse(request)
-		conn.Write(response.Parse())
+		fmt.Print(request)
+		// response := NewResponse(request)
+		// conn.Write(response.Parse())
 		fmt.Println("write called")
 	}
 }
